699e7f5960dbe578eaa53989b0d97481b083f26e
Michael Buck <mhbuck@gmail.com>
2012-09-03 16:13:02 +0200

    Initial Commit

---
+++ DocumentCommentParser.Console/DTO/DocumentComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentCommentParser.TestConsole.DTO
+{
+    public class DocumentComment
+    {
+        public string CommentId { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Initials { get; set; }
+
+        public string CommentText { get; set; }
+
+        public string CommentedText { get; set; }
+    }
+}
---
+++ DocumentCommentParser.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DocumentCommentParser.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string templatePath = string.Format("{0}\\Template\\{1}", GetExecutionPath(), "Comments.docx");
+            byte[] documentByte = File.ReadAllBytes(templatePath);
+
+            DocumentParser parser = new DocumentParser();
+            var comments = parser.GetCommentsFromDocument(documentByte);
+
+            foreach (var comment in comments)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine(comment.CommentText);
+                Console.WriteLine();
+                Console.WriteLine(comment.CommentedText);
+                Console.WriteLine("----------------------------");
+            }
+            Console.WriteLine("-- Ended --");
+            Console.ReadLine();
+
+        }
+
+        private static string GetExecutionPath()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+    }
+}
