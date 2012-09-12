1857039444ac8d34ac14fc7089beea16cfc79cc4
Steve Vaubell <vaubell@yahoo.com>
2012-09-12 09:35:21 +0100

    Added Logic project and test project so that the logic can be tested

---
+++ DocumentCommentParser.Logic/DTO/DocumentComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DocumentCommentParser.Logic.DTO
+{
+    public class DocumentComment
+    {
+        public string CommentId { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Initials { get; set; }
+
+        public string CommentText { get; set; }
+
+        public string CommentedText { get; set; }
+    }
+}
---
+++ DocumentCommentParser.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DocumentCommentParser.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocumentCommentParser.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e686e1d2-abe3-4bf2-a215-11a1a91e7352")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
